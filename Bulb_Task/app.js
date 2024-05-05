76569d15233011c408f4955d37cd278b630e5c85
jeeteshkumar99 <jeeteshkumarsuthar2003@gmail.com>
2024-05-05 17:07:49 +0500

    bulbTask

---
+++ Bulb_Task/app.js
@@ -0,0 +1,17 @@
+
+function onBulb(){
+    var getBulb = document.getElementById('bulb')
+    getBulb.src = 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/MSNBC/Components/Photo/_new/081209-light-bulb-03.jpg '
+    getBulb.setAttribute('width','370px')
+}
+
+function offBulb(){
+    var getBulb = document.getElementById('bulb')
+    getBulb.src = 'https://www.shutterstock.com/image-illustration/3d-illustration-light-bulb-isolated-600nw-749587456.jpg'
+    getBulb.setAttribute('width','400px')
+
+}
+
+
+
+
